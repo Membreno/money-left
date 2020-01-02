78624745915abe71d1a54aa24224aa0d6bf9236a
Pedro M <membrep@gmail.com>
2020-01-01 16:29:42 -0800

    updated addBill html

--- static/js/app.js
+++ static/js/app.js
@@ -1,23 +1,31 @@
 import BillList from './modules/billList.js';
-const billListEl = document.querySelector(".monthly__expense-details");
+// const billListEl = document.querySelector(".monthly__expense-details");
 const openModalTriggerEl = document.querySelector(".trigger");
 const closeModalEl = document.querySelector(".close-exit");
+const closeModal = document.querySelector(".close-exit-button");
 const modalEl = document.querySelector(".add-bill-modal");
+const billInputs = document.querySelectorAll('.bill-input');
 
-if (billListEl) {
+if (openModalTriggerEl) {
   BillList.init();
-  main();
+  addBillDisplay();
 }
-function main() {
+function addBillDisplay() {
   openModalTriggerEl.addEventListener("click", function () {
     modalEl.classList.add("open");
   });
   closeModalEl.addEventListener("click", function () {
     modalEl.classList.remove("open");
+    billInputs.forEach(elem => elem.value = '');
+  });
+  closeModal.addEventListener("click", function () {
+    modalEl.classList.remove("open");
+    billInputs.forEach(elem => elem.value = '');
   });
   window.addEventListener("click", function (event) {
     if (event.target === modalEl) {
       modalEl.classList.remove("open");
+      billInputs.forEach(elem => elem.value = '');
     }
   })
 }
