92124016a257048f2a3b5f778155bdbadf6e6573
Pedro M <membrep@gmail.com>
2020-01-13 11:13:56 -0800

    added autoIndex to server file and set to false

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,14 +1,4 @@
 const mongoose = require('mongoose');
-const path = require('path');
-const fs = require('fs');
-
-// Connecting out models to mongoose
-// let models_path = path.join(__dirname, '../models');
-// fs.readdirSync(models_path).forEach(function (file) {
-//   if (file.indexOf('.js') >= 0) {
-//     require(models_path + '/' + file);
-//   }
-// });
 
 // Connect to Mongo...
 // Set the option for autoIndex to false to prevent duplicate key error
