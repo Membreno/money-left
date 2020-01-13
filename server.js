3f8743b2fa2d3e0825efce485a591e5dd1306c70
Pedro M <membrep@gmail.com>
2020-01-13 01:26:33 -0800

    fully incorporated passport for login/registration/password_reset

--- server.js
+++ server.js
@@ -49,4 +49,4 @@ require('./server/config/mongoose.js');
 require('./server/config/routes.js')(app); // Passing the app as a input for our routes function
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, _ => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, _ => console.log(`Server started on port ${PORT}...`));
