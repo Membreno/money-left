0275f120fa276c8b8d6f1437ef5c6c4b4e0cea27
Pedro M <membrep@gmail.com>
2020-01-15 12:06:13 -0800

    changed mongo uri link

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Connect to Mongo...
 // Set the option for autoIndex to false to prevent duplicate key error
-mongoose.connect(process.env.MONGOURA, {
+mongoose.connect(process.env.MONGOURI, {
    useUnifiedTopology: true,
    useNewUrlParser: true,
    useCreateIndex: true,
