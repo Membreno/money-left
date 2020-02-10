55886425c5a31457c37d82dbbabfa1de8a25cf23
Pedro M <membrep@gmail.com>
2020-02-10 14:28:34 -0800

    Successfully sent email with sendgrid

--- server/controllers/reset.js
+++ server/controllers/reset.js
@@ -3,57 +3,69 @@ const User = require('../models/USER'),
       nodemailer = require('nodemailer'),
       crypto = require('crypto');
 
+const sgMail = require('@sendgrid/mail');
+const msg = {
+  to: 'test@example.com',
+  from: 'test@example.com',
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+
 module.exports = {
   // RESET PASSWORD
   forgot_password: function (req, res, next) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    sgMail.send(msg);
+
     async.waterfall([
-      function (done) {
-        crypto.randomBytes(20, function (err, buf) {
-          var token = buf.toString('hex');
-          done(err, token);
-        });
-      },
-      function (token, done) {
-        User.findOne({
-          username: req.body.email
-        }, function (err, user) {
-          if (!user) {
-            req.flash('error', 'No account with that email address exists.');
-            return res.redirect('/reset');
-          }
+      // function (done) {
+      //   crypto.randomBytes(20, function (err, buf) {
+      //     var token = buf.toString('hex');
+      //     done(err, token);
+      //   });
+      // },
+      // function (token, done) {
+      //   User.findOne({
+      //     username: req.body.email
+      //   }, function (err, user) {
+      //     if (!user) {
+      //       req.flash('error', 'No account with that email address exists.');
+      //       return res.redirect('/reset');
+      //     }
 
-          user.resetPasswordToken = token;
-          user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+      //     user.resetPasswordToken = token;
+      //     user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
 
-          user.save(function (err) {
-            done(err, token, user);
-          });
-        });
-      },
-      function (token, user, done) {
-        var smtpTransport = nodemailer.createTransport({
-          service: 'Gmail',
-          auth: {
-            user: 'understandurtech@gmail.com',
-            pass: process.env.GMAILPW
-          }
-        });
-        console.log('*************', user.email)
-        var mailOptions = {
-          to: user.username,
-          from: 'MoneyLeft Project',
-          subject: 'MoneyLeft Password Reset',
-          text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-            'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-            'http://' + req.headers.host + '/reset/' + token + '\n\n' +
-            'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-        };
-        smtpTransport.sendMail(mailOptions, function (err) {
-          console.log('mail sent');
-          req.flash('success_msg', 'An e-mail has been sent to ' + user.username + ' with further instructions.');
-          done(err, 'done');
-        });
-      }
+      //     user.save(function (err) {
+      //       done(err, token, user);
+      //     });
+      //   });
+      // },
+      // function (token, user, done) {
+      //   var smtpTransport = nodemailer.createTransport({
+      //     service: 'Gmail',
+      //     auth: {
+      //       user: 'understandurtech@gmail.com',
+      //       pass: process.env.GMAILPW
+      //     }
+      //   });
+      //   console.log('*************', user.email)
+      //   var mailOptions = {
+      //     to: user.username,
+      //     from: 'MoneyLeft Project',
+      //     subject: 'MoneyLeft Password Reset',
+      //     text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+      //       'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+      //       'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+      //       'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+      //   };
+      //   smtpTransport.sendMail(mailOptions, function (err) {
+      //     console.log('mail sent');
+      //     req.flash('success_msg', 'An e-mail has been sent to ' + user.username + ' with further instructions.');
+      //     done(err, 'done');
+      //   });
+      // }
     ], function (err) {
       if (err){
         return next(err);
